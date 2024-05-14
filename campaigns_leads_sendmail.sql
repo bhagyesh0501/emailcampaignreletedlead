29a52076c8dc791bec585b6d466c18e478e93d41
bhagyesh0501 <156882013+bhagyesh0501@users.noreply.github.com>
2024-05-14 11:05:42 +0530

    Add files via upload

---
+++ campaigns_leads_sendmail.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 10, 2024 at 12:08 PM
+-- Server version: 8.0.31
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vcrm2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaigns_leads_sendmail`
+--
+
+DROP TABLE IF EXISTS `campaigns_leads_sendmail`;
+CREATE TABLE IF NOT EXISTS `campaigns_leads_sendmail` (
+  `campaignid` int NOT NULL,
+  `leadid` int NOT NULL,
+  `sendmail` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
